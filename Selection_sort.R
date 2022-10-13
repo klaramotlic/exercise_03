8c8db3f38617495cf2729a5839efd22e2a67426b
Klara Motlickova <xmotli04@vutbr.cz>
2022-10-13 10:00:05 +0200

    adding IndexOfMin and SelectionSort function

---
+++ Selection_sort.R
@@ -0,0 +1,29 @@
+array <- c(2,7,1,46,8)
+IndexOfMin <- function(array,first,last){
+  index <- first
+  for (k in (first+1):last){
+    if (array[k] < array[index]){
+      index <- k
+    }
+  }
+print(index)
+}
+
+IndexOfMin(array,1,5)
+
+
+SelectionSort <- function(a, n){
+  for (i in 1:(n-1)){
+    j <- IndexOfMin(a, i, n)
+    a[i] <- a[j] + a[i]
+    a[j] <- a[i] - a[j]
+    a[i] <- a[i] - a[j]
+  }
+print(a)
+}
+
+a <- c(4,8,6,10,1,2)
+SelectionSort(a,6)
+
+
+
